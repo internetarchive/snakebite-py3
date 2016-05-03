8a4662e502e0c3f88f7aab5be47d072ee83884fd
Kirk Leon Guerrero <kirk.leonguerrero@gmail.com>
2016-05-02 23:46:04 -0700

    Insert protobuf path in sys.path, fix protobuf absolute imports

--- snakebite/protobuf/__init__.py
+++ snakebite/protobuf/__init__.py
@@ -12,3 +12,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
